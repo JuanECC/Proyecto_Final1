cdb45dc8cb963a099c216f472b88000ab75e79be
JuanECC <gatospro2000@gmai.com>
2025-11-28 12:44:38 -0600

    feat: texto dinámico del botón

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const themeToggle = document.getElementById("themeToggle");
 themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
     document.body.classList.toggle("dark-mode");
+
+     if (document.body.classList.contains("light-mode")) {
+        themeToggle.textContent = "Modo Oscuro";
+    } else {
+        themeToggle.textContent = "Modo Claro";
+    }
+});
